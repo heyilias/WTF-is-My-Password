04fbc445870749e4129c20e534b3c3f5ee4586c5
heyilias <ilias7576@gmail.com>
2021-04-29 16:57:42 +0000

    size changed

--- WTF-is-My-Password/Form1.Designer.cs
+++ WTF-is-My-Password/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace WTF_is_My_Password
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 435);
+            this.ClientSize = new System.Drawing.Size(800, 350);
             this.Name = "Form1";
             this.Text = "WTF is My Password";
             this.ResumeLayout(false);
